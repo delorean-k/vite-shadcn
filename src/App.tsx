26134f928d592b263bab636a082f6a89ae76415a
delorean-k <58541319+delorean-k@users.noreply.github.com>
2025-02-07 16:14:39 -0800

    demochart works on main.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
 import { Button } from '@/components/ui/button';
+import { useState } from 'react';
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  const handleClick = () => {
+    setCounter(counter + 1);
+  };
+
   return (
     <>
       <div className="p-5">
-        <Button>Click me</Button>
+        <Button onClick={handleClick}>Click me {counter}</Button>
+      </div>
+      <div className='p-5'>
+        The counter is: {counter}
+      </div>
+      <div className='p-5'>
+        <Button onClick={() => setCounter(0)}>Reset</Button>
+      </div>
+      <div>
+        
       </div>
     </>
   );
